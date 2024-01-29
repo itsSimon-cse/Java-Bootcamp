ee00c3f43095f93f5bdb142fb1d52ef277e69206
itsSimon-cse <itssimon.cse@gmail.com>
2024-01-29 12:15:18 +0530

    add2

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        int passengers=15;
+        System.out.println(passengers);
+
+        int busTickets=passengers;
+        System.out.println(busTickets);
+
+       // int max=21474836478;
+       
+    }
+    
+}
